f0d4f3e9708d51539cb52c94285b69cb992771ee
mahinour30 <mahinour1233@gmail.com>
2021-01-12 22:12:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './App.css';
 
 const App = ()=> {
+
+  const App_ID= '8386114c';
+  const App_KEY = '12b9dcc058067a9f34feec4632df8292';
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${App_ID}&app_key=${App_KEY}`;
+
+
   return (
     <div className="App">
       <h1>Hello React</h1>
