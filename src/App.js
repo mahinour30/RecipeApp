8e2b0c4c7ae25a2176646c7702b220ca847632fc
mahinour30 <mahinour1233@gmail.com>
2021-01-12 22:06:43 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = ()=> {
   return (
     <div className="App">
       <h1>Hello React</h1>
